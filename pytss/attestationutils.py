d51f6a078c596eba46da4351c4a42209395ab994
rpanchal1996 <rudreshp@fb.com>
2020-05-05 10:01:37 -0700

    xrange fix

--- pytss/attestationutils.py
+++ pytss/attestationutils.py
@@ -358,7 +358,7 @@ def mgf1(mgf_seed, mask_len, hash_class=hashlib.sha1):
     if mask_len > 0x10000:
         raise ValueError('mask too long')
     T = ''
-    for i in xrange(0, integer_ceil(mask_len, h_len)):
+    for i in range(0, integer_ceil(mask_len, h_len)):
         C = i2osp(i, 4)
         T = T + hash_class(mgf_seed + C).digest()
     return bytearray(T[:mask_len])
